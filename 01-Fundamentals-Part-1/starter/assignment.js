185d2c65761b860f859f9e9c561f219711451d78
Zacarias Paclibar <zacariaspaclibar@gmail.com>
2026-02-24 22:20:30 +0800

    answer to the assignment 1

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,8 @@
+let country = "Philippines";
+let continent = "Asia";
+let population = "117.7 Millions";
+
+console.log(country, continent, population);
+console.log(country);
+console.log(continent);
+console.log(population);
